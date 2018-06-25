10670b618c569e14d7adb100e7fc1dc452232069
Jon Choi <choi.jonathanh@gmail.com>
2018-06-25 08:39:25 -0700

    Prog bar edits

--- src/bar.rs
+++ src/bar.rs
@@ -2,19 +2,25 @@ use indicatif::{ProgressBar, ProgressStyle};
 
 static PBAR_FMT: &'static str = "{msg} {spinner:.green} {percent}% [{wide_bar:.cyan/blue}] {bytes}/{total_byes} eta: {eta}";
 
-pub fn create_progress_bar(msg: &str, length: Option<u64>) -> ProgressBar {
-    let progbar = match length {
-        Some(len) => ProgressBar::new(len),
-        None => ProgressBar::new_spinner(),
-    };
-
-    progbar.set_message(msg);
-    if length.is_some() {
-        progbar.set_style(ProgressStyle::default_bar().template(PBAR_FMT).progress_chars("=> "));
-    } else {
-        progbar.set_style(ProgressStyle::default_spinner());
+pub fn create_progress_bar(quiet_mode: bool, msg: &str, length: Option<u64>) -> ProgressBar {
+    let bar = match quiet_mode {
+        true => ProgressBar::hidden(),
+        false => {
+            match length {
+                Some(len) => ProgressBar::new(len),
+                None => ProgressBar::new_spinner(),
+            };
+        }
     }
 
-    progbar
+    progbar.set_message(msg);
+    match length.is_some() {
+        true => progbar.set_style(ProgressStyle::default_bar()
+                                  .template(PBAR_FMT)
+                                  .progress_chars("=> "));
+        false => progbar.set_style(ProgressStyle::default_spinner());
+    };
+
+    bar
 }
 
